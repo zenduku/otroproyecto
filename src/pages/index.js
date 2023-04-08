3880f8cbde4a797e6c66de9ccde8f516cedefb89
Juan <juandavid19950730@gmail.com>
2023-04-08 17:09:38 -0400

    cambios dia 8/04/2023

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,18 @@ import styles from 'Zenduku/styles/Home.module.css'
 import { Divider, Input,  } from '@chakra-ui/react'
 import {BarraPrincipal} from "../containers/BarraPrincipal";
 import React from "react";
-import { Flex, Square, Text, Button, Select} from '@chakra-ui/react'
+import { Flex, Square, Text, Button, Select, Spacer, Textarea, Switch} from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer} from '@chakra-ui/react'
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure} from '@chakra-ui/react'
 import { FormControl, FormLabel, FormErrorMessage, FormHelperText} from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 export default function Home() {
 
   const [currentView, setCurrentView] = React.useState("Inicio")
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
 
   return (
     <>
@@ -39,35 +41,55 @@ export default function Home() {
                       <h1>Linea Nueva!</h1>
                       <Button onClick={onOpen}>Trigger modal</Button>
 
-                      <Modal onClose={onClose} isOpen={isOpen} isCentered size='xl'>
+                      <Modal onClose={onClose} isOpen={isOpen} isCentered size='4xl'>
                         <ModalOverlay />
                         <ModalContent>
                           <ModalHeader>Modal Title</ModalHeader>
                           <ModalCloseButton />
                           <ModalBody>
-                            <Flex size='100%'>
-                              <Flex>
                                 <FormControl isRequired
                                   border='1px' borderColor='gray.200'
-                                  borderRadius='10' p={5} alignItems="center" justifyContent="space-between"
+                                  borderRadius='10' h='100%' p={3} alignItems="center" justifyContent="space-between"
                                 >
-                                  <FormLabel >Documento de Identidad</FormLabel>
-                                  <Flex>
-                                    <Select mr={2} w='22%'>
-                                      <option value='V'>V</option>
-                                      <option value='E'>E</option>
-                                      <option value='P'>P</option>
-                                      <option value='J'>J</option>
-                                    </Select>
-                                    <Input placeholder='Ingrese los digitos del documento'/>
-                                  </Flex>
-                                  <Input placeholder='ingrese los digitos del documento'/>
+                                  <Grid
+                                    templateRows='repeat(4, 1fr)'
+                                    templateColumns='repeat(2, 1fr)'
+                                    gap={4}
+                                  >
+                                    <GridItem rowSpan={4} colSpan={1}>
+                                      <FormLabel htmlFor='inputDocumentoIdentidad'>Documento de Identidad</FormLabel>
+                                      <Flex>
+                                        <Select mr={2} w='22%' mb={2}>
+                                          <option value='V'>V</option>
+                                          <option value='E'>E</option>
+                                          <option value='P'>P</option>
+                                          <option value='J'>J</option>
+                                        </Select>
+                                        <Input id='inputDocumentoIdentidad' placeholder='Ingrese los digitos del documento'/>
+                                      </Flex>
+                                      <FormLabel htmlFor='inputTelefonoCliente'>Telefono del cliente</FormLabel>
+                                      <Input id='inputTelefonoCliente' placeholder='ingrese los digitos del documento' mb={2}/>
+                                      <Flex >
+                                        <Switch id='switch-eSIM' mr={2} mt={0.5}/>
+                                        <FormLabel htmlFor='switch-eSIM'>
+                                          Venta con eSIM
+                                        </FormLabel>
+                                      </Flex>
+                                      <FormLabel htmlFor='textareaComentario'>Comentario</FormLabel>
+                                      <Textarea
+                                        id='textareaComentario'
+                                        placeholder='Here is a sample placeholder'
+                                        size='xs'
+                                        resize='none'
+                                      />
+                                    </GridItem>
+
+                                    <GridItem rowSpan={4} colSpan={1}>
+                                      <FormLabel htmlFor='inputFormaPago'>Forma de pago</FormLabel>
+                                      <Input placeholder='Ingrese los digitos del documento' id='inputFormaPago'/>
+                                    </GridItem>
+                                  </Grid>
                                 </FormControl>
-                              </Flex>
-                              
-                              <Input w='50%' placeholder='Ingrese los digitos del documento' />
-                            </Flex>
-                                                        
                           </ModalBody>
                           <ModalFooter mr={10}>
                             <Button onClick={onClose}>Close</Button>
